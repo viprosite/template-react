21386ecc4417d02e1296496510c8acadde0d3ee1
王保清 <viprosite@outlook.com>
2023-07-19 14:40:19 +0800

    'init'

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/header/Header';
+export default function App(){
+   return (
+    <>
+        <Header />
+        <article>
+           1 content...
+        </article>
+    </>
+   )
+   
+}
